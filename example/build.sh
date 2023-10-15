8ce452f6852f381e96ad7cb3de7f09134d91b242
aleksandersh <alex-shuttle@yandex.ru>
2023-10-15 20:36:46 +0200

    update example build script

--- example/build.sh
+++ example/build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 FILENAME="$1"
-FILEPATH="$TUI_PACK_CONFIG_DIR/bin/$FILENAME"
+WORKING_DIR="$TUI_PACK_CONFIG_DIR/../"
+FILEPATH="$WORKING_DIR/bin/$FILENAME"
 rm -f "$FILEPATH"
-(cd "$TUI_PACK_CONFIG_DIR" && go build -o "$FILEPATH") || {
+(cd "$WORKING_DIR" && go build -o "$FILEPATH") || {
     echo "failed to build"
     exit 1
 }
-(cd "$TUI_PACK_CONFIG_DIR/bin/" && tar -czvf "$FILENAME.tar.gz" "$FILENAME") || {
+(cd "$WORKING_DIR/bin/" && tar -czvf "$FILENAME.tar.gz" "$FILENAME") || {
     echo "failed to archive"
     exit 1
 }
