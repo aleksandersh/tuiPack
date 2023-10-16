14246612aa096119135a4d84bdad70d765a9b416
aleksandersh <alex-shuttle@yandex.ru>
2023-10-16 09:27:34 +0200

    improve events observer

--- tui/content_controller.go
+++ tui/content_controller.go
@@ -9,10 +9,10 @@ import (
 	"github.com/rivo/tview"
 )
 
-type event int16
+type controllerEvent int16
 
 const (
-	eventResetFilter event = iota
+	eventResetFilter controllerEvent = iota
 	eventActivateFilter
 	eventCancelFilter
 	eventFinishFilter
@@ -20,12 +20,12 @@ const (
 )
 
 type contentController struct {
-	events chan event
+	events chan controllerEvent
 }
 
 func newContentController(ctx context.Context, app *tview.Application, commandsView *tview.List, filterView *tview.TextArea, commands []config.Command) *contentController {
 	initialCommands := mapCommandsToViewItems(commands)
-	events := make(chan event, 100)
+	events := make(chan controllerEvent, 100)
 	contentState := contentController{events: events}
 	go processControlsEvents(ctx, app, commandsView, filterView, initialCommands, events)
 	return &contentState
@@ -51,90 +51,88 @@ func (cs *contentController) RefreshContentByFilter() {
 	cs.events <- eventRefreshContentByFilter
 }
 
-func processControlsEvents(ctx context.Context, app *tview.Application, commandsView *tview.List, filterView *tview.TextArea, initialCommands []commandViewItem, events chan event) {
+func processControlsEvents(ctx context.Context, app *tview.Application, commandsView *tview.List, filterView *tview.TextArea, initialCommands []commandViewItem, events chan controllerEvent) {
 	populateCommandsView(ctx, app, commandsView, initialCommands)
 
 	filteredCommands := initialCommands
 	isFilterViewActive := false
 	currentText := ""
 
+	observeEvents(ctx, events, func(event controllerEvent) {
+		switch event {
+		case eventResetFilter:
+			if isFilterViewActive {
+				return
+			}
+			if currentText == "" {
+				if commandsView.GetCurrentItem() != 0 {
+					commandsView.SetCurrentItem(0)
+					app.Draw()
+				}
+				return
+			}
+			currentText = ""
+			filterView.SetText("", true)
+			absoluteCommandIndex := getAbsoluteCommandIndex(commandsView, filteredCommands)
+			filteredCommands = initialCommands
+			commandsView.Clear()
+			populateCommandsView(ctx, app, commandsView, filteredCommands)
+			commandsView.SetCurrentItem(absoluteCommandIndex)
+			app.Draw()
+		case eventActivateFilter:
+			if isFilterViewActive {
+				return
+			}
+			isFilterViewActive = true
+			filterView.SetDisabled(false)
+			app.SetFocus(filterView)
+			app.Draw()
+		case eventCancelFilter:
+			if !isFilterViewActive {
+				return
+			}
+			isFilterViewActive = false
+			currentText = ""
+			filterView.SetDisabled(true)
+			filterView.SetText("", true)
+			absoluteCommandIndex := getAbsoluteCommandIndex(commandsView, filteredCommands)
+			filteredCommands = initialCommands
+			commandsView.Clear()
+			populateCommandsView(ctx, app, commandsView, filteredCommands)
+			commandsView.SetCurrentItem(absoluteCommandIndex)
+			app.SetFocus(commandsView)
+			app.Draw()
+		case eventFinishFilter:
+			if !isFilterViewActive {
+				return
+			}
+			isFilterViewActive = false
+			filterView.SetDisabled(true)
+			app.SetFocus(commandsView)
+			app.Draw()
+		case eventRefreshContentByFilter:
+			newText := filterView.GetText()
+			if currentText == newText {
+				return
+			}
+			currentText = newText
+			absoluteCommandIndex := getAbsoluteCommandIndex(commandsView, filteredCommands)
+			commands, index := filterCommands(initialCommands, absoluteCommandIndex, newText)
+			filteredCommands = commands
+			commandsView.Clear()
+			populateCommandsView(ctx, app, commandsView, commands)
+			commandsView.SetCurrentItem(index)
+		}
+	})
+}
+
+func observeEvents(ctx context.Context, events chan controllerEvent, handler func(event controllerEvent)) {
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		case event := <-events:
-			if event == eventResetFilter {
-				if isFilterViewActive {
-					continue
-				}
-				if currentText == "" {
-					if commandsView.GetCurrentItem() != 0 {
-						commandsView.SetCurrentItem(0)
-						app.Draw()
-					}
-					continue
-				}
-				currentText = ""
-				filterView.SetText("", true)
-				absoluteCommandIndex := getAbsoluteCommandIndex(commandsView, filteredCommands)
-				filteredCommands = initialCommands
-				commandsView.Clear()
-				populateCommandsView(ctx, app, commandsView, filteredCommands)
-				commandsView.SetCurrentItem(absoluteCommandIndex)
-				app.Draw()
-				continue
-			}
-			if event == eventActivateFilter {
-				if isFilterViewActive {
-					continue
-				}
-				isFilterViewActive = true
-				filterView.SetDisabled(false)
-				app.SetFocus(filterView)
-				app.Draw()
-				continue
-			}
-			if event == eventCancelFilter {
-				if !isFilterViewActive {
-					continue
-				}
-				isFilterViewActive = false
-				currentText = ""
-				filterView.SetDisabled(true)
-				filterView.SetText("", true)
-				absoluteCommandIndex := getAbsoluteCommandIndex(commandsView, filteredCommands)
-				filteredCommands = initialCommands
-				commandsView.Clear()
-				populateCommandsView(ctx, app, commandsView, filteredCommands)
-				commandsView.SetCurrentItem(absoluteCommandIndex)
-				app.SetFocus(commandsView)
-				app.Draw()
-				continue
-			}
-			if event == eventFinishFilter {
-				if !isFilterViewActive {
-					continue
-				}
-				isFilterViewActive = false
-				filterView.SetDisabled(true)
-				app.SetFocus(commandsView)
-				app.Draw()
-				continue
-			}
-			if event == eventRefreshContentByFilter {
-				newText := filterView.GetText()
-				if currentText == newText {
-					continue
-				}
-				currentText = newText
-				absoluteCommandIndex := getAbsoluteCommandIndex(commandsView, filteredCommands)
-				commands, index := filterCommands(initialCommands, absoluteCommandIndex, newText)
-				filteredCommands = commands
-				commandsView.Clear()
-				populateCommandsView(ctx, app, commandsView, commands)
-				commandsView.SetCurrentItem(index)
-				continue
-			}
+			handler(event)
 		}
 	}
 }
